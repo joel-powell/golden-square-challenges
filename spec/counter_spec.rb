f3cd6175c9a11d2087445f971e58c6b0a693392d
Chris Leech <chrisleech@monzo.com>
2023-01-17 14:39:13 +0000

    added counter.rb and added 2 tests

---
+++ spec/counter_spec.rb
@@ -0,0 +1,17 @@
+require 'counter'
+
+RSpec.describe Counter do
+  it "returns a counter that hasn't been added to" do
+    count = Counter.new
+    result = count.report
+    expect(result).to eq "Counted to 0 so far."
+  end
+
+  it "returns a counter equal to 3" do
+    count = Counter.new
+    count.add(1)
+    count.add(2)
+    result = count.report
+    expect(result).to eq "Counted to 3 so far."
+  end
+end
