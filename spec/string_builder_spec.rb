698a51aea199a6daaee8be518dd324669cfbabb0
Joel Powell <joelhpowell@icloud.com>
2023-01-17 14:51:46 +0000

    add string_builder.rb and 5 tests

---
+++ spec/string_builder_spec.rb
@@ -0,0 +1,37 @@
+require "string_builder"
+
+RSpec.describe StringBuilder do
+  it "returns an empty string" do
+    string = StringBuilder.new
+    result = string.output
+    expect(result).to eq ""
+  end
+
+  it "returns length of empty string" do
+    string = StringBuilder.new
+    result = string.size
+    expect(result).to eq 0
+  end
+
+  it "returns the given string" do
+    string = StringBuilder.new
+    string.add("hello")
+    result = string.output
+    expect(result).to eq "hello"
+  end
+
+  it "returns two combined strings" do
+    string = StringBuilder.new
+    string.add("hello")
+    string.add("world")
+    result = string.output
+    expect(result).to eq "helloworld"
+  end
+
+  it "returns length of given string" do
+    string = StringBuilder.new
+    string.add("hello")
+    result = string.size
+    expect(result).to eq 5
+  end
+end
