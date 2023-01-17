95ec9aa5eabefd57fe102630d9b9aab9492386b5
Joel Powell <joelhpowell@icloud.com>
2023-01-17 15:02:51 +0000

    add gratitudes.rb and 3 tests

---
+++ spec/gratitudes_spec.rb
@@ -0,0 +1,24 @@
+require "gratitudes"
+
+RSpec.describe Gratitudes do
+  it "returns no gratitudes" do
+    gratitudes = Gratitudes.new
+    result = gratitudes.format
+    expect(result).to eq "Be grateful for: "
+  end
+
+  it "returns a single gratitude" do
+    gratitudes = Gratitudes.new
+    gratitudes.add("food")
+    result = gratitudes.format
+    expect(result).to eq "Be grateful for: food"
+  end
+
+  it "returns two gratitudes" do
+    gratitudes = Gratitudes.new
+    gratitudes.add("food")
+    gratitudes.add("water")
+    result = gratitudes.format
+    expect(result).to eq "Be grateful for: food, water"
+  end
+end
