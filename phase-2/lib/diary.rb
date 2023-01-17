2a5596fce696b899e80ae3b8c15b0e59a0ceb97b
Chris Leech <chrisleech@monzo.com>
2023-01-17 17:29:50 +0000

    created count_words method and added 3 passing tests

--- phase-2/lib/diary.rb
+++ phase-2/lib/diary.rb
@@ -1,13 +1,17 @@
 def make_snippet(string)
-  string_array = string.split
-  word_count = string_array.length
-  first_five_words = string_array[0..4].join(" ")
-  first_five_words << "..." if word_count > 5
-  first_five_words
+  # string_array = string.split
+  # word_count = string_array.length
+  # first_five_words = string_array[0..4].join(" ")
+  # first_five_words << "..." if word_count > 5
+  # first_five_words
 
-  #   if string.split.length > 5
-  #     split = string.split
-  #     return split[0..4].join(' ') + "..."
-  #   end
-  #   string
+  if string.split.length > 5
+    split = string.split
+    return split[0..4].join(' ') + "..."
+  end
+  string
+end
+
+def count_words(string)
+  string.split.length
 end
--- phase-2/spec/diary_spec.rb
+++ phase-2/spec/diary_spec.rb
@@ -36,3 +36,26 @@ RSpec.describe "make_snippet method" do
     end
   end
 end
+
+RSpec.describe "count_words method" do
+  context "When a string is empty" do
+    it "returns 0" do
+      string = count_words("")
+      expect(string).to eq 0
+    end
+  end
+
+  context "When the string has 1 word" do
+    it "returns 1" do
+      string = count_words("hello")
+      expect(string).to eq 1
+    end
+  end
+
+  context "When the string has 2 words" do
+    it "returns 2" do
+      string = count_words("hello world")
+      expect(string).to eq 2
+    end
+  end
+end
