3e13cd61066e8a15eeacd6593b78924abf6175cf
Joel Powell <joelhpowell@icloud.com>
2023-01-18 20:54:43 +0000

    add encode_decode exercise

---
+++ phase-2/lib/encode_decode.rb
@@ -0,0 +1,15 @@
+def encode(plaintext, key)
+  cipher = key.chars.uniq + (("a".."z").to_a - key.chars)
+  ciphertext_chars = plaintext.chars.map do |char|
+    (65 + cipher.find_index(char)).chr
+  end
+  ciphertext_chars.join
+end
+
+def decode(ciphertext, key)
+  cipher = key.chars.uniq + (("a".."z").to_a - key.chars)
+  plaintext_chars = ciphertext.chars.map do |char|
+    cipher[char.ord - 65]
+  end
+  plaintext_chars.join
+end
