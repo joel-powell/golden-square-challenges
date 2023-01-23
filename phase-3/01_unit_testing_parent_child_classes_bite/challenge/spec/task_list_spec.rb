76fe3e2bc5ac436d080c323fdc890671753347f7
Joel Powell <joelhpowell@icloud.com>
2023-01-23 10:30:20 +0000

    add phase 3 mocking bites challenge

---
+++ phase-3/01_unit_testing_parent_child_classes_bite/challenge/spec/task_list_spec.rb
@@ -0,0 +1,15 @@
+require "task_list"
+
+RSpec.describe TaskList do
+  it "initially has an empty list" do
+    task_list = TaskList.new
+    expect(task_list.all).to eq []
+  end
+
+  it "is initially not all complete" do
+    task_list = TaskList.new
+    expect(task_list.all_complete?).to eq false
+  end
+
+  # Unit test `#all` and `#all_complete?` behaviour
+end
